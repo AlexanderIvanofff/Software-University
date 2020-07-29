5c75110967479aff59bc1ce485ff528db6b987ad
alexander.ivanofff@gmail.com <68683375+AlexanderIvanofff@users.noreply.github.com>
2020-07-29 15:50:51 +0300

    add new problem.

---
+++ Trains.py
@@ -0,0 +1,25 @@
+wagons = int(input())
+train = [0] * wagons
+
+while True:
+    command = input()
+    if command == 'End':
+        break
+
+    tokens = command.split()
+    instruction = tokens[0]
+
+    if instruction == 'add':
+        count = int(tokens[1])
+        train[-1] += count
+    elif instruction == 'insert':
+        index = int(tokens[1])
+        count = int(tokens[2])
+        train[index] += count
+    elif instruction == 'leave':
+        index = int(tokens[1])
+        count = int(tokens[2])
+        train[index] -= count
+
+print(train)
+
